255d1efeef8694eae3325c0eefe457a116c8e665
Olga <olga.ermolaeva@big-vienna.com>
2020-09-17 09:17:48 +0200

    Root commit. Added basic dependencies

---
+++ analyse_/src/main/java/io/threatened/species/analyse/AnalyseThreatenedSpeciesApplication.java
@@ -0,0 +1,13 @@
+package io.threatened.species.analyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyseThreatenedSpeciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyseThreatenedSpeciesApplication.class, args);
+	}
+
+}
---
+++ analyse_/src/test/java/io/threatened/species/analyse/AnalyseThreatenedSpeciesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.threatened.species.analyse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyseThreatenedSpeciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
